6ebd36877a931879b415cb1143cb15327766e508
victoria valencia <116645345+victoriaavalee@users.noreply.github.com>
2023-09-02 01:48:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require ('express');
 const app = express ();
-const PORT= process.env.PORT || 8080;
+const PORT= process.env.PORT || 8080
 const path= require ('path');
 const publicFolderPath=path.resolve(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
-app.listen (8080, () =>{
-    console.log ('MercadoLiebre corriendo en el puerto 8080');
+app.listen (PORT, () => {
+    console.log (`[server] corriendo en el puerto: ${PORT}`);
 });
-
+//Rutas
 app.get ('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
-
 app.get ('/creaTuCuenta', (req, res)=>{
-    res.sendFile(path.join (__dirname, './views/creaTuCuenta.html'))
+    res.sendFile(path.join (__dirname, './views/CreaTuCuenta.html'))
 });
 
 app.get ('/ingresa', (req, res)=>{
